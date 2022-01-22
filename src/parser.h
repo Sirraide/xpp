2128a63ed59b829c58dd7f3ae2738b7932bc71a8
Sirraide <aeternalmail@gmail.com>
2022-01-22 19:22:30 +0100

    \Define args

--- src/parser.h
+++ src/parser.h
@@ -17,13 +17,14 @@ enum struct TokenType : Char {
     EndOfFile       = Eof,
     CommandSequence = U'\\',
     Macro,
+    MacroArg,
+    Whitespace,
     LineComment = U'%',
     GroupBegin  = U'{',
     GroupEnd    = U'}',
 };
 
 struct Node : public TokenBase<char32_t, TokenType, SourceLocationBase<>> {
-    // std::vector<Node> children;
     String Str() const override;
 };
 
@@ -35,37 +36,56 @@ struct ReplacementRules {
     std::vector<std::pair<String, String>>     processed;
 };
 
+struct Macro {
+    NodeList              replacement;
+    std::vector<NodeList> delimiters;
+
+    Macro() = default;
+    Macro(NodeList replacement);
+    Macro(std::vector<NodeList> delimiters, NodeList replacement);
+};
+
 struct Parser : public AbstractLexer {
     const Clopts& opts;
     explicit Parser(const Clopts& opts);
-    FILE*                      output_file;
-    std::map<String, NodeList> macros;
-    ReplacementRules           rep_rules;
-    ReplacementRules           raw_rep_rules;
-    NodeList                   tokens;
-    U64                        group_count = 0;
-    std::queue<Node>           lookahead_queue;
-    String                     processed_text;
+    FILE*                   output_file;
+    std::map<String, Macro> macros;
+    ReplacementRules        rep_rules;
+    ReplacementRules        raw_rep_rules;
+    NodeList                tokens;
+    U64                     group_count = 0;
+    std::queue<Node>        lookahead_queue;
+    String                  processed_text;
+    bool                    last_token_was_cs = false;
 
-    void     LexLineComment();
-    void     LexCommandSequence();
-    void     LexText();
-    void     NextToken();
-    void     Parse();
-    void     EmitCommandSequence();
-    void     Emit();
-    void     Expect(TokenType type);
-    void     ParseToken();
-    NodeList ParseGroup(bool keep_closing_brace = false);
-    void     ParseCommandSequence();
-    void     ConstructText(NodeList& nodes);
-    void     ParseSequence();
-    void     ProcessReplacement(NodeList& lst);
-    void     ProcessReplacementRules();
-    String   AsTextNode(const NodeList& lst);
-    void     ApplyReplacementRules(String& str);
-    void     ApplyRawReplacementRules();
-    void     HandleReplace();
+    void                  LexLineComment();
+    void                  LexCommandSequence();
+    void                  LexText();
+    void                  NextToken();
+    void                  Parse();
+    void                  EmitCommandSequence();
+    void                  Emit();
+    void                  Expect(TokenType type);
+    void                  ParseToken();
+    NodeList              ParseGroup(bool keep_closing_brace = false);
+    void                  ParseCommandSequence();
+    void                  ConstructText(NodeList& nodes);
+    void                  ParseSequence();
+    void                  ProcessReplacement(NodeList& lst);
+    void                  ProcessReplacementRules();
+    String                AsTextNode(const NodeList& lst);
+    void                  ApplyReplacementRules(String& str);
+    void                  ApplyRawReplacementRules();
+    void                  HandleReplace();
+    String                ReplaceReadUntilBrace();
+    void                  NextNonWhitespaceToken();
+    void                  SkipCharsUntilIfWhitespace(Char c);
+    void                  HandleDefine();
+    std::vector<NodeList> ParseMacroArgs();
+    static std::string    TokenTypeToString(TokenType type);
+    static void           MergeTextNodes(NodeList& lst);
+    void                  LexMacroArg();
+    void                  HandleMacroExpansion();
 };
 
 String StringiseType(const Node& token);
